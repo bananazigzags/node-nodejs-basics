4631350c183700fe28f89c64f91072681c1c0962
Maria <bananazigzags@gmail.com>
2024-01-29 02:24:42 +0300

    feat: zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createGunzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { archivePath, originPath } from "./paths.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(archivePath);
+  const writeStream = createWriteStream(originPath);
+  const unzip = createGunzip();
+  try {
+    await pipeline(readStream, unzip, writeStream)
+  } catch(err) {
+    console.log(err)
+  }
 };
 
 await decompress();
