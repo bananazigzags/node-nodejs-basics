723c96fc9f6ab4b95ce22ce450e3f29cf52a1413
Maria <bananazigzags@gmail.com>
2024-01-29 02:16:14 +0300

    feat: fash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "node:fs";
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = createReadStream(
+    new URL("files/fileToCalculateHashFor.txt", import.meta.url)
+  );
+  const hash = createHash("sha256");
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
