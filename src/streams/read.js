e7b80c29a526f89a6eb4370ef55b4a47f36fdf9d
Maria <bananazigzags@gmail.com>
2024-01-29 02:31:42 +0300

    feat: streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from "node:fs";
+
+const fileToRead = new URL("files/fileToRead.txt", import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const fileContent = createReadStream(fileToRead);
+  fileContent.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from "node:fs";
+import * as readline from "node:readline/promises";
+
+const fileToWrite = new URL("files/fileToWrite.txt", import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "What should I write?\n",
+  });
+
+  const writeStream = createWriteStream(fileToWrite);
+
+  rl.prompt();
+
+  rl.on("line", (input) => {
+    writeStream.write(input);
+    writeStream.on("error", (err) => {
+      console.log(err);
+    });
+    rl.close();
+  }).on("close", () => {
+    writeStream.end();
+    process.exit();
+  });
 };
 
 await write();
