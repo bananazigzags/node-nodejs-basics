8aa7b986c936ca8d9ba1f43429399d264d5f9ae5
Maria <bananazigzags@gmail.com>
2024-01-29 02:12:13 +0300

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argsLen = args.length / 2;
+  let output = "";
+
+  for (let i = 0; i < argsLen; i++) {
+    let argsPair = args.splice(0, 2);
+    output += `${argsPair[0].slice(2)} is ${argsPair[1]}`;
+    if (args.length > 0) output += ", ";
+  }
+
+  console.log(output);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+  let result = "";
+
+  const rssVars = Object.keys(process.env).filter((variable) =>
+    variable.startsWith("RSS_")
+  );
+
+  const numRss = rssVars.length;
+
+  rssVars.forEach((variable, index) => {
+    result += `${variable}=${process.env[variable]}`;
+    if (index !== numRss - 1) result += "; ";
+  });
+
+  console.log(result);
 };
 
 parseEnv();
